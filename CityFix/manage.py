2123b11d11e58b320a1df294007bc914174b36f9
hoppa7 <bobgon050@gmail.com>
2026-03-02 18:27:05 +0200

    Chore: renamed project

--- CityFix/manage.py
+++ CityFix/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unnamed.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CityFix.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
