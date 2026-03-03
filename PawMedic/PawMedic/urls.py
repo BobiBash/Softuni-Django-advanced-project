bb0ffed5eee6729abe95f6a6fb63dd8b94da7992
hoppa7 <bobgon050@gmail.com>
2026-03-03 22:26:26 +0200

    Chore: set up django_tailwind_cli and project structure

--- PawMedic/PawMedic/urls.py
+++ PawMedic/PawMedic/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CityFix project.
+URL configuration for PawMedic project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
--- PawMedic/PawMedic/wsgi.py
+++ PawMedic/PawMedic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CityFix project.
+WSGI config for PawMedic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CityFix.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PawMedic.settings')
 
 application = get_wsgi_application()
