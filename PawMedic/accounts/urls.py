5278a7f13bacbf2b64ff87251f8614214e0e0926
hoppa7 <bobgon050@gmail.com>
2026-03-10 16:21:31 +0200

    Feat: logout functionality, profile_card, is_authenticated checks for register

--- PawMedic/accounts/urls.py
+++ PawMedic/accounts/urls.py
@@ -4,10 +4,12 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('register/', TemplateView.as_view(template_name='accounts/register.html'), name='register'),
+    path('register/', views.RegisterOptionsView.as_view(), name='register'),
     path('register-user/', views.RegisterView.as_view(), name='register_user'),
     path('register-vet/', views.RegisterVetView.as_view(), name='register_vet'),
     path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('confirm-email/<str:key>/', views.ConfirmEmailView.as_view(), name='confirm-email'),
     path('confirm-vet', TemplateView.as_view(template_name='accounts/confirm_vet.html'), name='confirm-vet'),
     path('email-confirmation',
--- PawMedic/accounts/views.py
+++ PawMedic/accounts/views.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .choices import PawMedicUserType
 from .forms import RegistrationForm, LoginForm
@@ -21,6 +22,12 @@ class RegisterView(CreateView):
     template_name = 'accounts/register_user.html'
     form_class = RegistrationForm
     success_url = reverse_lazy('home')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(*args, **kwargs)
+
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -51,6 +58,12 @@ class RegisterVetView(CreateView):
     form_class = RegistrationForm
     success_url = reverse_lazy('home')
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(*args, **kwargs)
+
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['role'] = PawMedicUserType.VET
@@ -68,9 +81,18 @@ class RegisterVetView(CreateView):
         user.save()
         return redirect('confirm-vet')
 
+class RegisterOptionsView(TemplateView):
+    template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(*args, **kwargs)
+
 class LoginUserView(LoginView):
     template_name = 'accounts/login.html'
     form_class = LoginForm
+    next_page = 'home'
 
 
 
@@ -84,7 +106,19 @@ class ConfirmEmailView(View):
         return redirect('successful-email-confirmation')
 
 
+class ProfileView(LoginRequiredMixin, TemplateView):
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        print(user, user.email, user.role)
+        context['profile'] = user
+        return context
+
+    def get_template_names(self):
+        if self.request.user.role == PawMedicUserType.OWNER:
+            return ['accounts/owner_profile.html']
+        return ['accounts/vet_profile.html']
 
 
 
