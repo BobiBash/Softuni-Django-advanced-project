ad440110e1687eba1b0dd60d513a7f1f22e51fa6
hoppa7 <bobgon050@gmail.com>
2026-03-19 23:28:15 +0200

    add profile navbar and pets page

--- pets/models.py
+++ pets/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
-# class Pet(models.Model):
-#     owner = models.ForeignKey()
-#     name = models.CharField(max_length=255)
+from accounts.models import PawMedicUser
+
+
+class Pet(models.Model):
+    owner = models.ForeignKey(PawMedicUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    pet_photo = models.ImageField(null=True, blank=True)
